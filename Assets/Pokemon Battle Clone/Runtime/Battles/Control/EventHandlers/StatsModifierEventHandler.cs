8422a405a64084941601537512f8b0f369af3cc0
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-03-15 15:10:06 +0100

    feat: add pokemon name to stats modifier dialog

--- Assets/Pokemon Battle Clone/Runtime/Battles/Control/EventHandlers/StatsModifierEventHandler.cs
+++ Assets/Pokemon Battle Clone/Runtime/Battles/Control/EventHandlers/StatsModifierEventHandler.cs
@@ -3,7 +3,6 @@ using Pokemon_Battle_Clone.Runtime.Battles.Domain;
 using Pokemon_Battle_Clone.Runtime.Battles.Domain.Events;
 using Pokemon_Battle_Clone.Runtime.Battles.Infrastructure.Dialogs;
 using Pokemon_Battle_Clone.Runtime.Stats.Domain;
-using UnityEngine;
 
 namespace Pokemon_Battle_Clone.Runtime.Battles.Control.EventHandlers
 {
@@ -23,38 +22,41 @@ namespace Pokemon_Battle_Clone.Runtime.Battles.Control.EventHandlers
             var view = battleEvent.ApplyToTarget ? 
                 _battleContext.GetTeamView(battleEvent.ActionSide.Opposite())
                 : _battleContext.GetTeamView(battleEvent.ActionSide);
+            var pokemonName = battleEvent.ApplyToTarget ? battleEvent.TargetName : battleEvent.UserName;
+            
             view.SetStatModifier(battleEvent.Modifier);
-            await DisplayMessages(battleEvent.Modifier);
+            await DisplayMessages(pokemonName, battleEvent.Modifier);
         }
 
-        private async Task DisplayMessages(StatsModifier modifier)
+        private async Task DisplayMessages(string pokemonName, StatsModifier modifier)
         {
             if (modifier.AttackLevel != 0)
-                await _dialogDisplayer.DisplayAsync(GetMessage("Attack", modifier.AttackLevel));
+                await _dialogDisplayer.DisplayAsync(GetMessage(pokemonName, "attack", modifier.AttackLevel));
             if (modifier.DefenseLevel != 0)
-                await _dialogDisplayer.DisplayAsync(GetMessage("Defense", modifier.DefenseLevel));
+                await _dialogDisplayer.DisplayAsync(GetMessage(pokemonName, "defense", modifier.DefenseLevel));
             if (modifier.SpcAttackLevel != 0)
-                await _dialogDisplayer.DisplayAsync(GetMessage("Sp. Atk", modifier.SpcAttackLevel));
+                await _dialogDisplayer.DisplayAsync(GetMessage(pokemonName, "sp. atk", modifier.SpcAttackLevel));
             if (modifier.SpcDefenseLevel != 0)
-                await _dialogDisplayer.DisplayAsync(GetMessage("Sp. Defense", modifier.SpcDefenseLevel));
+                await _dialogDisplayer.DisplayAsync(GetMessage(pokemonName, "sp. defense", modifier.SpcDefenseLevel));
             if (modifier.SpeedLevel != 0)
-                await _dialogDisplayer.DisplayAsync(GetMessage("Speed", modifier.SpeedLevel));
+                await _dialogDisplayer.DisplayAsync(GetMessage(pokemonName, "speed", modifier.SpeedLevel));
         }
 
-        private string GetMessage(string statistic, int level)
+        private string GetMessage(string pokemonName, string statistic, int level)
         {
+            var prefix = $"{pokemonName}'s {statistic}";
             if (level == 1)
-                return $"{statistic} rose!";
+                return $"{prefix} rose!";
             if (level == 2)
-                return $"{statistic} rose sharply!";
+                return $"{prefix} rose sharply!";
             if (level >= 3)
-                return $"{statistic} rose drastically!";
+                return $"{prefix} rose drastically!";
             if (level == -1)
-                return $"{statistic} fell!";
+                return $"{prefix} fell!";
             if (level == -2)
-                return $"{statistic} harshly fell!";
+                return $"{prefix} harshly fell!";
             if (level <= -3)
-                return $"{statistic} severely fell";
+                return $"{prefix} severely fell";
             
             return string.Empty;
         }
