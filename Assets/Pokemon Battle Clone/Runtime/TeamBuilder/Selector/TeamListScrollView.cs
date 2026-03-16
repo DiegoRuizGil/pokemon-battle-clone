25dfdf3534e35e460a7dfb6ac0fa5d43bae48f58
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-03-16 13:24:11 +0100

    feat: team list scroll view

---
+++ Assets/Pokemon Battle Clone/Runtime/TeamBuilder/Selector/TeamListScrollView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Pokemon_Battle_Clone.Runtime.Database;
+using UnityEngine;
+
+namespace Pokemon_Battle_Clone.Runtime.TeamBuilder.Selector
+{
+    public class TeamListScrollView : MonoBehaviour
+    {
+        [SerializeField] private List<TeamConfig> teamConfigs;
+
+        [Header("UI")]
+        [SerializeField] private TeamSelector teamSelectorPrefab;
+        [SerializeField] private GameObject content;
+        
+        private void Awake()
+        {
+            foreach (var teamConfig in teamConfigs)
+            {
+                var selector = Instantiate(teamSelectorPrefab, content.transform);
+                selector.Init(teamConfig);
+            }
+        }
+    }
+}
