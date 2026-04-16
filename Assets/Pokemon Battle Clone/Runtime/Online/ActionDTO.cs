bc3cb80d2bbded80df07b4ab7ed6817509924d40
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-04-16 16:02:13 +0200

    feat: action dto

---
+++ Assets/Pokemon Battle Clone/Runtime/Online/ActionDTO.cs
@@ -0,0 +1,22 @@
+﻿using Fusion;
+
+namespace Pokemon_Battle_Clone.Runtime.Online
+{
+    public enum ActionType : byte
+    {
+        Move, Swap
+    }
+    
+    public struct ActionDTO : INetworkStruct
+    {
+        public ActionType Type;
+        /// <summary>
+        /// Either move index (move action) or the pokemon index (swap action)
+        /// </summary>
+        public int Index;
+        /// <summary>
+        /// Only relevant for swap actions
+        /// </summary>
+        public NetworkBool WithdrawFirst;
+    }
+}
