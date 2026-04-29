5668917c920b9eb4bb8cb569b6d44adc0ea88224
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-04-29 19:07:20 +0200

    refactor: lobby panel UI rework

---
+++ Assets/Pokemon Battle Clone/Runtime/Online/Lobby/UI/CopySessionCode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Pokemon_Battle_Clone.Runtime.Online.Lobby.UI
+{
+    public class CopySessionCode : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI sessionCodeText;
+        [SerializeField] private string copiedText;
+        [SerializeField] private float copiedTextDuration;
+
+        private bool _showingCopiedText;
+        
+        public void Copy()
+        {
+            if (_showingCopiedText) return;
+            
+            GUIUtility.systemCopyBuffer = sessionCodeText.text;
+            
+            StopAllCoroutines();
+            StartCoroutine(ShowCopiedText());
+        }
+
+        private IEnumerator ShowCopiedText()
+        {
+            _showingCopiedText = true;
+            var code = sessionCodeText.text;
+            sessionCodeText.text = copiedText;
+            yield return new WaitForSeconds(copiedTextDuration);
+            sessionCodeText.text = code;
+            _showingCopiedText = false;
+        }
+    }
+}
--- Assets/Pokemon Battle Clone/Runtime/Online/Lobby/UI/GameInfoDisplayer.cs
+++ Assets/Pokemon Battle Clone/Runtime/Online/Lobby/UI/GameInfoDisplayer.cs
@@ -12,14 +12,14 @@ namespace Pokemon_Battle_Clone.Runtime.Online.Lobby.UI
 
         public void Display(GameState state)
         {
-            sessionNameText.text = $"Session: {state.SessionCode}";
+            sessionNameText.text = state.SessionCode;
             
             var p1Status = state.LocalPlayer.IsReady ? "Ready" : "Waiting";
-            p1StatusText.text = $"Local Player: {p1Status}";
+            p1StatusText.text = $"You: {p1Status}";
             
             var p2Status = state.RemotePlayer.IsReady ? "Ready" : "Waiting";
             p2StatusText.text = state.RemotePlayer.IsPresent ?
-                $"Remote Player: {p2Status}" :
+                $"Rival: {p2Status}" :
                 "Waiting for rival...";
         }
     }
