15f53ce28c2e99a08ffd74dcc370d892958f1821
Diego Ruiz Gil <diegoruiz037@gmail.com>
2026-04-29 17:09:32 +0200

    feat: LobbyResult class

---
+++ Assets/Pokemon Battle Clone/Runtime/Online/Lobby/LobbyResult.cs
@@ -0,0 +1,11 @@
+﻿namespace Pokemon_Battle_Clone.Runtime.Online.Lobby
+{
+    public class LobbyResult
+    {
+        public bool Success { get; private set; }
+        public string ErrorMessage { get; private set; }
+
+        public static LobbyResult Ok() => new() { Success = true };
+        public static LobbyResult Fail(string message) => new() { Success = false, ErrorMessage = message };
+    }
+}
